28066d969d17f9fd68ea2395f30c8f952ea3345e
Raihan Islam <iraihan705@gmail.com>
2026-08-03 15:06:15 +0600

    Remove unnessecary db error handle

--- src/server.js
+++ src/server.js
@@ -60,21 +60,6 @@ const server = app.listen(PORT, () => {
 });
 
 
-// Handle unhandled promise rejections (e.g., database connection errors)
-process.on("unhandledRejection", (err) => {
-  console.error("Unhandled Rejection:", err);
-  server.close(async () => {
-    await disconnectDB();
-    process.exit(1);
-  });
-});
-
-// Handle uncaught exceptions
-process.on("uncaughtException", async (err) => {
-  console.error("Uncaught Exception:", err);
-  await disconnectDB();
-  process.exit(1);
-});
 
 // Graceful shutdown
 process.on("SIGTERM", async () => {
